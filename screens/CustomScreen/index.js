b44dbff2b0ca3f64a46b2b82442d28ad3968c1d1
Aline Tavares <alaisgomes@gmail.com>
2023-05-11 14:31:10 -0400

    Create index.js

---
+++ screens/CustomScreen/index.js
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from 'react-native';
+function HelloWorld() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.paragraph, styles.colors]}>
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  colors: {
+    color: "#FF0000"
+  }
+});
+
+export default HelloWorld;
