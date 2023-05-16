76bec80ff700434810394b4f3750773f21ccd71f
Aline Tavares <alaisgomes@gmail.com>
2023-05-16 12:32:00 -0400

    Create styles.js

---
+++ screens/SimpleScreen/styles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const custom = StyleSheet.create({
+  deep: {
+    color: "#4c4f69", fontSize: 12, fontWeight: "bold"
+  },
+  font: {
+    color: '#7287fd',
+  },
+  background: {
+    backgroundColor: "#ccd0da"
+  }
+});
+
+export const deepStyle = custom.deep;
