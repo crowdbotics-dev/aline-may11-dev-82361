94e7190a265a703c662f9e19a4a8591fa27b7739
Aline Tavares <alaisgomes@gmail.com>
2023-05-18 10:17:54 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled4/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+export default function App() {
+  return <View style={styles.container}>
+      <Text>Style me.</Text>
+    </View>;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  }
+});
