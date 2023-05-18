dacb849da14a70a535a7c9115fae44f6e77f4fe2
Aline Tavares <alaisgomes@gmail.com>
2023-05-18 10:27:12 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/CustomScreen/index.js
+++ screens/CustomScreen/index.js
@@ -1,9 +1,10 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
 
 function HelloWorld() {
   return <View style={styles.container}>
       <Text style={[styles.paragraph, styles.colors]}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+        Change code in the editor and watch it change on your phone! Save to get
+        a shareable url.
       </Text>
     </View>;
 }
@@ -11,15 +12,15 @@ function HelloWorld() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
     padding: 8
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "center"
   },
   colors: {
     color: "#FF0000"
